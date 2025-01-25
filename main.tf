19424930298cd8f66397346a801d930c4ee6a39d
Eric Hochhalter <erichochhalter@users.noreply.github.com>
2025-01-24 22:08:18 -0600

    Hotfix/v0.1.1 (#7)
    Allows for a manual override of the Log Group name as an argument

--- main.tf
+++ main.tf
@@ -7,8 +7,9 @@ module "sfn_error_notification_context" {
 locals {
   step_functions = {
     for id, sfn in var.step_functions : id => {
-      name           = local.enabled ? split(":stateMachine:", sfn.arn)[1] : ""
       arn            = sfn.arn
+      name           = local.enabled ? split(":stateMachine:", sfn.arn)[1] : ""
+      log_group_name = local.enabled ? coalesce(sfn.log_group_name, "/aws/vendedlogs/states/${split(":stateMachine:", sfn.arn)[1]}") : ""
       sqs_queue_name = local.enabled ? "${split(":stateMachine:", sfn.arn)[1]}-dlq" : ""
     }
   }
